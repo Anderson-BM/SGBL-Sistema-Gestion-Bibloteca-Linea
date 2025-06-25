8036142e1cf89c2cce40641b94417fdc68983763
Anderson Batista mateo <andersonbatistamateo@gmail.com>
2025-06-25 17:15:29 -0400

    Aplicando Identity

---
+++ SGBL.Infrastructure.Identity/Entities/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGBL.Infrastructure.Identity.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public required string FirstName { get; set; }
+
+        public required string LastName { get; set; }
+
+        public string? IdentificationNumber { get; set; }
+    }
+}
---
+++ SGBL.Infrastructure.Identity/ServiceRegistration.cs
@@ -0,0 +1,34 @@
+﻿//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+//using SGBL.Infrastructure.Identity.Context;
+
+//namespace SGBL.Infrastructure.Identity
+//{
+//    public static class ServiceRegistration
+//    {
+//        public static void AddIdentityLayer(this IServiceCollection services, IConfiguration configuration)
+//        {
+//            #region Contexts
+//            if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+//            {
+//                services.AddDbContext<IdentityContext>(options => options.UseInMemoryDatabase("ApplicationDb"));
+//            }
+//            else
+//            {
+//                services.AddDbContext<IdentityContext>(options =>
+//                {
+//                    options.EnableSensitiveDataLogging();
+//                    options.UseSqlServer(configuration.GetConnectionString("IdentityConnection"),
+//                    m => m.MigrationsAssembly(typeof(IdentityContext).Assembly.FullName));
+
+//                });
+//            }
+//            #endregion
+
+//            #region Identity
+
+//            #endregion
+//        }
+//    }
+//}
